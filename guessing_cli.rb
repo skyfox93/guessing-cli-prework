00464f195eb9b56b6f4ccfad14c5e40307c60322
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-15 00:09:38 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   correct = rand(1..6)
   input=""
-  loop while input!="exit"
+   while input!="exit"
     input=gets.chomp
      if(input=="exit")
        puts "Goodbye!"
